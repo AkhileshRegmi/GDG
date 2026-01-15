183b57eb6bd650e211d6a8395c8944d7ca163110
J0HNDO033 <regmiakhu@gmail.com>
2026-01-15 12:12:57 +0530

    password hashing logic fix and made first user admin

--- backend/crud/user.py
+++ backend/crud/user.py
@@ -10,8 +10,8 @@ def get_user_by_email(db, email):
     return db.query(User).filter(User.email == email).first()
 
 def create_user(db, user):
-    hashed_password = get_password_hash(user.password)
-    role = "user"    
+    hashed_password = pwd_context.hash(user.password)
+    role = "admin" if db.query(User).count() == 0 else "user"
     db_user = User(email=user.email, hashed_password=hashed_password, role=role)
     db.add(db_user)
     db.commit()
